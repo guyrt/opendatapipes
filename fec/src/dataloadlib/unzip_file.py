4ef76065e6a819572e495d3b9681b7beac245d3b
Tommy Guy <riguy@microsoft.com>
2022-04-25 17:50:47 -0700

    unzip works

--- fec/src/dataloadlib/unzip_file.py
+++ fec/src/dataloadlib/unzip_file.py
@@ -1,5 +1,6 @@
-from distutils.command.upload import upload
-from io import BytesIO
+from os import listdir
+from os.path import isfile, join, getsize
+import tempfile
 import zipfile
 from .blob_helpers import get_blob_client, get_service_client
 
@@ -14,17 +15,38 @@ def unzip_and_upload(unzip_request_source):
     unzip_request_root = "/".join(unzip_request_source.split('/')[:-1])
     
     zip_file_contents = bc.download_blob().readall()
-    zf = zipfile.ZipFile(BytesIO(zip_file_contents))
+
+    # save file to disk
+    zip_fp = tempfile.TemporaryFile()
+    zip_fp.write(zip_file_contents)
+    zip_fp.flush()
+    zip_fp.seek(0)
+    
+    unzip_tempdir = tempfile.TemporaryDirectory()
+    with zipfile.ZipFile(zip_fp) as zipObj:
+        zipObj.extractall(unzip_tempdir.name)
+
+    all_files = [f for f in listdir(unzip_tempdir.name) if isfile(join(unzip_tempdir.name, f))]
+    non_files = [f for f in listdir(unzip_tempdir.name) if not isfile(join(unzip_tempdir.name, f))]
+    if non_files:
+        raise Exception(f"Found directories which is not supported: {', '.join(non_files)}")
+
     created_files = []
     total_bytes = 0
-    for file in zf.filelist:
-        local_file_name = f"{unzip_request_root}/{file.filename}"
-        total_bytes += file.file_size
+    for rawfilename in all_files:
+        filename = join(unzip_tempdir.name, rawfilename)
+        fh = open(filename, 'rb')
+        remote_file_name = f"{unzip_request_root}/{rawfilename}"
+        total_bytes += getsize(filename)
 
-        created_files.append(local_file_name)
-        contents = zf.read(file.filename)
+        created_files.append(remote_file_name)
 
-        upload_client = service_client.get_blob_client(container=upload_container, blob=local_file_name)
-        upload_client.upload_blob(contents)
+        upload_client = service_client.get_blob_client(container=upload_container, blob=remote_file_name)
+        upload_client.upload_blob(fh, overwrite=True)
+        fh.close()
 
     return created_files, total_bytes
+
+
+if __name__ == "__main__":
+    print(unzip_and_upload("electronic/20220321.zip"))
