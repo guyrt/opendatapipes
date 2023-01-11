e2ef424e450e3cbb0bf3d6b27620f0ce88c04b7d
Tommy Guy <riguy@microsoft.com>
2023-01-10 19:57:52 -0800

    updated the runners for efficiency

--- fec/src/fecPipeline/local_day_runner.ps1
+++ fec/src/fecPipeline/local_day_runner.ps1
@@ -1,36 +1,56 @@
 # local runner to cheaply iterate over days 1 at at time
 
-$run_date = 20220108
+$startString = $args[0]
+$numDays = $args[1]
 
-# hangs up if you try to save output.
-# az ml job create --file .\fecUnzipToMergePipeline.yaml --resource-group rg-fecdata --workspace fecaml --set inputs.run_date=$run_date
+$num_failures = 0
 
-# $run_obj = cat /tmp/az.json | ConvertFrom-Json
-# $run_name = $run_obj.name
+$startDate = [datetime]::parseexact($startString, 'yyyyMMdd', $null)
+$endDate = $startDate.AddDays($numDays)
 
-$run_name = "shy_calypso_4cvyh1g8yv"
+$days = New-Object Collections.Generic.List[string]
 
-echo "Running $run_date named $run_name";
-
-function get_status($name) {
-    $az_status_obj = az ml job show --name $run_name --resource-group rg-fecdata --workspace fecaml | ConvertFrom-Json
-
-    $az_status = $az_status_obj.status
-    return $az_status
+while ($startDate -lt $endDate) {
+    $days.Add($startDate.ToString('yyyyMMdd'))
+    $startDate = $startDate.AddDays(1)
 }
 
-$az_status = get_status($run_name)
+foreach  ($run_date in $days) {
+    echo "Start on $run_date";
+    # hangs up if you try to save output.
+    az ml job create --file .\fecUnzipToMergePipeline.yaml --resource-group rg-fecdata --workspace fecaml --set inputs.run_date=$run_date > /tmp/az.json
 
-$last_date_check = Get-Date
-while ($az_status -eq "Running") {
-    $now = Get-Date
-    if (($now - $last_date_check).TotalMinutes -gt 1) {
-        echo "$az_status at $now"
-        $last_date_check = Get-Date
+    $run_obj = cat /tmp/az.json | ConvertFrom-Json
+    $run_name = $run_obj.name
+
+    echo "Running $run_date named $run_name";
+
+    function get_status($name) {
+        $az_status_obj = az ml job show --name $run_name --resource-group rg-fecdata --workspace fecaml | ConvertFrom-Json
+
+        $az_status = $az_status_obj.status
+        return $az_status
     }
-    Start-Sleep -s 30
-    $az_status_obj = az ml job show --name $run_name --resource-group rg-fecdata --workspace fecaml | ConvertFrom-Json
-    $az_status = get_status($run_name)
-}
 
-echo $az_status
+    $az_status = get_status($run_name)
+
+    $last_date_check = Get-Date
+    while ($az_status -eq "Running") {
+        $now = Get-Date
+        if (($now - $last_date_check).TotalMinutes -gt 5) {
+            echo "$az_status at $now"
+            $last_date_check = Get-Date
+        }
+        Start-Sleep -s 60
+        $az_status_obj = az ml job show --name $run_name --resource-group rg-fecdata --workspace fecaml | ConvertFrom-Json
+        $az_status = get_status($run_name)
+    }
+
+    echo "$run_date had $az_status"
+    if ($az_status -eq "Failed") {
+        $num_failures = $num_failures + 1
+        if ($num_failures -gt 3) {
+            break;
+        }
+    }
+}
--- fec/src/fecPipeline/statusUpdates/write_complete.py
+++ fec/src/fecPipeline/statusUpdates/write_complete.py
@@ -15,12 +15,16 @@ parser.add_argument("--status_out_path", type=str)
 
 args = parser.parse_args()
 
+print(f"Unzipped fec_files is {args.unzipped_fec_files}")
+
 input_file = join(args.unzipped_fec_files, "dates.txt")
 datepatterns = [line.strip() for line in open(input_file, 'r')]
 
 output_folder_uri = args.status_out_path
 
 print(f"Running on output_folder {output_folder_uri} and date {datepatterns}")
+
+#https://fecblob.blob.core.windows.net/sparkfilesystem/azureml/6516f387-d384-4221-a824-4453aab9a2ec/unzipped_fec_files/dates.txt
 
 for datepattern in datepatterns:
     output_uri = join(output_folder_uri, f"{datepattern}.txt")
